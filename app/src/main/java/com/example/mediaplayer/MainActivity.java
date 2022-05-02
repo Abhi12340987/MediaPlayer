cb9f78b9187be603a1fd9f671ca5cdc56bd2b25a
Abhishek Parekh <abhishekparekh25@yahoo.ca>
2022-05-01 20:26:47 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    private Button playButton;
+    private SeekBar mediaSeekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.theme);
+        mediaSeekBar.setMax(mediaPlayer.getDuration());
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                int duration = mediaPlayer.getDuration();
+                String mDuration  = String.valueOf(duration/1000);
+
+                Toast.makeText(getApplicationContext(), "duration " + mDuration, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        playButton = (Button) findViewById(R.id.playButtonId);
+        mediaSeekBar = (SeekBar) findViewById(R.id.mSeekBar);
+
+        mediaSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                if (b) {
+                    mediaPlayer.seekTo(i);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+                    //stop and give option to start again
+                    pauseMusic();
+                }
+                else {
+                    playMusic();
+                }
+            }
+        });
+
+    }
+    public void pauseMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+            playButton.setText("Play");
+        }
+    }
+
+    public void playMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+            playButton.setText("Pause");
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+
+        }
+        super.onDestroy();
+    }
+}
+
